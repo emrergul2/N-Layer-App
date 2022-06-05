1b16ce85d07161c3e91f264698d4e58d33dbe09b
emrergul2 <emrergul2@gmail.com>
2022-06-05 22:52:55 +0300

    migrations

--- NLayer.Repository/Seeds/ProductFeatureSeed.cs
+++ NLayer.Repository/Seeds/ProductFeatureSeed.cs
@@ -13,12 +13,8 @@ namespace NLayer.Repository.Seeds
         public void Configure(EntityTypeBuilder<ProductFeature> builder)
         {
             builder.HasData(
-                new Product { Id = 1, CategoryId = 1, Name = "Pencil-1", Price = 200, Stock = 20, CreatedDate = DateTime.Now },
-                new Product { Id = 2, CategoryId = 1, Name = "Pencil-2", Price = 180, Stock = 25, CreatedDate = DateTime.Now },
-                new Product { Id = 3, CategoryId = 1, Name = "Pencil-3", Price = 160, Stock = 30, CreatedDate = DateTime.Now },
-                new Product { Id = 4, CategoryId = 1, Name = "Pencil-4", Price = 140, Stock = 35, CreatedDate = DateTime.Now },
-                new Product { Id = 5, CategoryId = 2, Name = "Book-1", Price = 120, Stock = 40, CreatedDate = DateTime.Now },
-                new Product { Id = 6, CategoryId = 2, Name = "Book-2", Price = 699, Stock = 40, CreatedDate = DateTime.Now }
+                new ProductFeature { Id = 1, ProductId = 1, Color = "red", Height = 200, Width = 200 },
+                new ProductFeature { Id = 2, ProductId = 2, Color = "black", Height = 200, Width = 100 }
             );
         }
     }
--- NLayer.Repository/Seeds/ProductSeed.cs
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -13,9 +13,13 @@ namespace NLayer.Repository.Seeds
         public void Configure(EntityTypeBuilder<Product> builder)
         {
             builder.HasData(
-                new ProductFeature { Id = 1, ProductId = 1, Color = "red", Height = 200, Width = 200 },
-                new ProductFeature { Id = 2, ProductId = 2, Color = "black", Height = 200, Width = 100 }
-          );
+                new Product { Id = 1, CategoryId = 1, Name = "Pencil-1", Price = 200, Stock = 20, CreatedDate = DateTime.Now },
+                new Product { Id = 2, CategoryId = 1, Name = "Pencil-2", Price = 180, Stock = 25, CreatedDate = DateTime.Now },
+                new Product { Id = 3, CategoryId = 1, Name = "Pencil-3", Price = 160, Stock = 30, CreatedDate = DateTime.Now },
+                new Product { Id = 4, CategoryId = 1, Name = "Pencil-4", Price = 140, Stock = 35, CreatedDate = DateTime.Now },
+                new Product { Id = 5, CategoryId = 2, Name = "Book-1", Price = 120, Stock = 40, CreatedDate = DateTime.Now },
+                new Product { Id = 6, CategoryId = 2, Name = "Book-2", Price = 699, Stock = 40, CreatedDate = DateTime.Now }
+            );
         }
     }
 }
