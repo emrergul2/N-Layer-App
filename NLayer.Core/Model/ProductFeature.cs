4cda2470c91d60d51a220a4497c05acea22dd2aa
emrergul2 <emrergul2@gmail.com>
2022-06-05 16:16:25 +0300

    entities

---
+++ NLayer.Core/Model/ProductFeature.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Model
+{
+    public class ProductFeature
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
