064ec1cd0d7109ffdf69a77bc7f212a5a7cf4fb4
emrergul2 <emrergul2@gmail.com>
2022-06-06 16:02:05 +0300

    AutoMapper

---
+++ NLayer.Service/Mapping/MapProfile.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using NLayer.Core.DTOs;
+
+using NLayer.Core.Model;
+namespace NLayer.Service.Mapping
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductFeature, ProductFeatureDto>().ReverseMap();
+            CreateMap<ProductUpdateDto, Product>();
+        }
+    }
+}
