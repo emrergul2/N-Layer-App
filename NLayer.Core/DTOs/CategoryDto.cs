0db6278a07c283768b8c5f8344b9cb466790f82b
emrergul2 <emrergul2@gmail.com>
2022-06-06 15:46:30 +0300

    DTOs

---
+++ NLayer.Core/DTOs/CategoryDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CategoryDto : BaseDto
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ NLayer.Core/DTOs/ProductDto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class ProductDto:BaseDto
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
